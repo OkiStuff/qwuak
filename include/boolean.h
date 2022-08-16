dc1a72a968f0aef97615d8d94fa1fcfb539ebdcd
Frankie A <40648091+OkiStuff@users.noreply.github.com>
2022-08-16 20:03:17 +0000

    added boolean and worked on router

---
+++ include/boolean.h
@@ -0,0 +1,7 @@
+#ifndef Q_BOOLEAN_H
+#define Q_BOOLEAN_H
+
+#define Q_TRUE 1
+#define Q_FALSE 0
+
+#endif // Q_BOOLEAN_H
--- include/router.h
+++ include/router.h
@@ -1,9 +1,32 @@
+/*
+    Qwuak Router
+
+    Requires:
+        <qwuak/router.h>
+*/
+
 #ifndef Q_ROUTER_H
 #define Q_ROUTER_H
+
+typedef void (*q_route_callback_t)(const char*);
+
+struct _q_route
+{
+    const char* route;
+    q_route_callback_t callback;
+};
+
+struct _q_router
+{
+    struct _q_route* routes; // expectes an allocated array with an allocated size of at least `sizeof(q_route_t)`
+    
+};
 
 #ifdef __cplusplus
 extern "c" {
 #endif // __cplusplus
+
+
 
 #ifdef __cplusplus
 }
